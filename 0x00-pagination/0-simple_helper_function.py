08e6c1d89598e7c92d3bcd2e6076b2998b9e20ab
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-25 17:46:56 +0300

    Implement index_range simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list of items
+
+    Args:
+        page (int): page number
+        page_size (int): number of items per page
+    
+    Returns:
+        tuple: a tuple of size two containing start index and end index
+    """
+    return ((page - 1) * page_size, page * page_size)
