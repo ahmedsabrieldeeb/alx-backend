63382ab630c3d262194980d6f14062f013987127
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-30 16:56:27 +0300

    refactor FIFOCache class and add docstrings

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,6 +6,10 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache (BaseCaching):
+    """
+    FIFOCache: class that inherits from BaseCaching
+    and is a caching system
+    """
     def put(self, key, item):
         """
         Add an item in the cache
