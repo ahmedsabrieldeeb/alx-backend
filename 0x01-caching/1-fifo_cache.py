cff39e88d4c56889bc3bbdcfaa3cdda906ebdfda
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-30 16:55:20 +0300

    Implement FIFO caching algorithm in FIFOCache class

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" 1-fifo_cache.py """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache (BaseCaching):
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        data = self.cache_data
+        if key is None or item is None:
+            return
+
+        if len(data) == self.MAX_ITEMS:
+            data.pop(next(iter(data)))
+
+        data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        data = self.cache_data
+        if key is None or key not in data:
+            return None
+
+        return data.get(key)
