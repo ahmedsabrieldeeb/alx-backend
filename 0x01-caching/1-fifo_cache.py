df61a877618fa956c1dbcda048130e89869f89e7
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-30 18:46:57 +0300

    refactor FIFOCache class to improve drop key logging

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,19 +10,37 @@ class FIFOCache (BaseCaching):
     FIFOCache: class that inherits from BaseCaching
     and is a caching system
     """
+    def __init__(self):
+        """
+        BaseCaching constructor
+        """
+        super().__init__()
+        self.insert_order = []  # FIFO queue
+
     def put(self, key, item):
         """
         Add an item in the cache
         """
-        data = self.cache_data
         if key is None or item is None:
             return
 
-        if len(data) == self.MAX_ITEMS:
-            drop_key = next(iter(data))
-            data.pop(drop_key)
-            print("DISCARD: {}".format(drop_key))
+        data = self.cache_data
+        queue = self.insert_order
 
+        if key in data:
+            # Move the key to the end to mark it as the most recently used
+            queue.remove(key)
+            queue.append(key)
+        else:
+            # If the cache is full, remove the oldest item
+            if len(data) == self.MAX_ITEMS:
+                oldest_key = queue.pop(0)
+                del data[oldest_key]
+                print(f"DISCARD: {oldest_key}")
+
+            queue.append(key)
+
+        # Update the cache data with the new item
         data[key] = item
 
     def get(self, key):
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,49 @@
 #!/usr/bin/python3
+""" 1-fifo_cache.py """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache (BaseCaching):
+    """
+    LIFOCache: class that inherits from BaseCaching
+    and is a caching system
+    """
+    def __init__(self):
+        """
+        BaseCaching constructor
+        """
+        super().__init__()
+        self.stack = []  # LIFO Stack
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        data = self.cache_data
+        stack = self.stack
+
+        if len(data) == self.MAX_ITEMS and key not in data:
+            last_key = stack.pop()  # Remove the last inserted key
+            del data[last_key]
+            print("DISCARD: {}".format(last_key))
+
+        if key in data:
+            stack.remove(key)  # Remove key if it already exists
+
+        data[key] = item
+        stack.append(key)  # Add key to the stack
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        data = self.cache_data
+        if key is None or key not in data:
+            return None
+
+        return data.get(key)
