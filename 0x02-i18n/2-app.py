2c5ac240fdfa87cb3729abc51abc6da4f39f0681
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-09-01 23:21:02 +0300

    chore: Update app.py and index.html files for basic flask app rendering with Babel support

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,36 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A simple flask app"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    """Configuration class for Babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Return the best matched language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Main page route"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
